0bba5c00b8e77f6d04e81166100c6272bbbc0e32
jsterling15 <joshua.sterling15@gmail.com>
2017-06-25 15:43:52 -0500

    First full working version pushed to GitHub
    Uploading present state of the game at the time GitHub started being used.

---
+++ Fribrary_v2.0/Assets/Scripts/Entities/Mobs/FollowingMob.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingMob : Entity {
+
+    public Entity following;
+    public float distance;
+
+    
+
+
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(following.GetComponent<Rigidbody2D>().transform.position.y > ((GetComponent<Rigidbody2D>().transform.position.y) + distance))
+        {
+            GetComponent<Rigidbody2D>().transform.position += Vector3.up * Time.deltaTime;
+        }
+        if (following.GetComponent<Rigidbody2D>().transform.position.y < ((GetComponent<Rigidbody2D>().transform.position.y) - distance))
+        {
+            GetComponent<Rigidbody2D>().transform.position += Vector3.down * Time.deltaTime;
+        }
+        if (following.GetComponent<Rigidbody2D>().transform.position.x > ((GetComponent<Rigidbody2D>().transform.position.x) + distance))
+        {
+            GetComponent<Rigidbody2D>().transform.position += Vector3.right * Time.deltaTime;
+        }
+        if (following.GetComponent<Rigidbody2D>().transform.position.x < ((GetComponent<Rigidbody2D>().transform.position.x) - distance))
+        {
+            GetComponent<Rigidbody2D>().transform.position += Vector3.left * Time.deltaTime;
+        }
+        if (health <= 0)
+        {
+            Die();
+        }
+        
+    }
+
+    public void Die() { }
+}
