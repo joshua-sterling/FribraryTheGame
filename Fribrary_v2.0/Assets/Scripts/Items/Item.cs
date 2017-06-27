7883e9337c891bed436466666ebb7dde8aba1836
jsterling15 <joshua.sterling15@gmail.com>
2017-06-26 19:10:22 -0500

    Notes added
    Added additional comments to most scripts

--- Fribrary_v2.0/Assets/Scripts/Items/Item.cs
+++ Fribrary_v2.0/Assets/Scripts/Items/Item.cs
@@ -2,18 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
+[System.Serializable]                       //serializable allows editor to see them
 public class Item : MonoBehaviour  {
     
-    public int itemID;
-    public string itemName;
-    public Texture2D itemIcon;
-    public string itemDescription;
-    public Sprite itemSprite;
-    public GameObject itemObject;
+    public int itemID;                      //identifier for the item
+    public string itemName;                 //item name
+    public Texture2D itemIcon;              //Texture for buttons
+    public string itemDescription;          //descritiption to be displayed
+    public Sprite itemSprite;               //Sprite to be displayed
+    public GameObject itemObject;           //game object to reference
    
 
-    public Item(int id, string name, Texture2D icon, string desc)
+    public Item(int id, string name, Texture2D icon, string desc)       //constructor
     {
         itemID = id;
         itemName = name;
