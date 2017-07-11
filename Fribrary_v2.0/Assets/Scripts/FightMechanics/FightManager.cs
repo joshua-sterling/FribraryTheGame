f89268792a8353a5b6fd177d9f5233431916a608
jsterling15 <joshua.sterling15@gmail.com>
2017-07-10 21:26:36 -0500

    Update Fight Mechanics
    Moved fight mechanics to fight manager.  Enemy now auto-responds after delay.  Created separate prefab and tag for enemy projectile.  Increased projectile speed.

--- Fribrary_v2.0/Assets/Scripts/FightMechanics/FightManager.cs
+++ Fribrary_v2.0/Assets/Scripts/FightMechanics/FightManager.cs
@@ -6,19 +6,55 @@ public class FightManager : MonoBehaviour {
 
     public static FightManager fm;
 
+    public FireProjectile enemyFire;
+    public FireProjectile playerFire;
+
+    bool enemyTurn = false;                     //is it the enemy's turn
+    bool playerTurn = false;
+
 	// Use this for initialization
 	void Start () {
-        
+        playerTurn = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-
+        if(enemyTurn) { enemyAttack(); }
 
 	}
 
+    public void enemyAttack()
+    {
+        if (enemyTurn)
+        {
+            enemyTurn = false;
+           
+            StartCoroutine(Waiting());
+
+            playerTurn = true;
+        }
+       
+    }
     
+    public void playerAttack()
+    {
+        if (playerTurn)
+        {
+            playerFire.shootProjectile();
+            playerTurn = false;           
+            enemyTurn = true;
+        }
+    }
+
+    IEnumerator Waiting()
+    {
+
+        yield return new WaitForSecondsRealtime(3);
+        enemyFire.shootProjectile();
+    }
+
+
 
 
 }
