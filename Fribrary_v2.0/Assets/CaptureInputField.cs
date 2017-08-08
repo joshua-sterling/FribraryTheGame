b82699e2a5686ab0e334d8c993a4aaf82d855afd
jsterling15 <joshua.sterling15@gmail.com>
2017-08-07 21:30:56 -0500

    Added Exit buttons, New game Name entry
    Added an exit button to main level and fight scene in order to exit the application.
    
    Added box to create a player name when creating a new game.  Placed validations on the data entered in this field.

---
+++ Fribrary_v2.0/Assets/CaptureInputField.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class CaptureInputField : MonoBehaviour {
+
+    public InputField usernameInput;                                    //identifies the input field where name will be entered
+    Regex alphaRegex = new Regex("[a-zA-Z]");                           //regular expression that identifies characters that are allowed
+
+    public void validateUserName()
+    {
+        MatchCollection result = alphaRegex.Matches(usernameInput.text);
+
+        if(result.Count == usernameInput.text.Length)
+        {            
+            GameController.controller.playerName = usernameInput.text;              //store the player name entered
+            SceneManager.LoadScene(1);                                              //load the Main Level scene
+        }
+        else
+        {
+            EditorUtility.DisplayDialog("Player Name Error!", "Player name can only contain letters!", "ok");
+        }
+        
+    }
+
+}
---
+++ Fribrary_v2.0/Assets/Scripts/ActivateGameObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateGameObject : MonoBehaviour {
+
+    public GameObject thingToActivate;
+
+    public void activate()
+    {
+        thingToActivate.SetActive(true);
+    }
+}
